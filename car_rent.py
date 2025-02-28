1451142047a9445d46a849b3784716a6a43f907b
JadHajali1 <jadhajali773@gmail.com>
2025-02-28 22:32:04 +0200

    Add getter and setter for rental price

--- car_rent.py
+++ car_rent.py
@@ -1,21 +1,25 @@
 class Vehicle:
-
-    brand = ""
-    model = ""
-    year = 0
-    rental_price_per_day = 0
-
     def __init__(self, brand, model, year, rental_price_per_day):
         self.brand = brand
         self.model = model
         self.year = year
-        self.rental_price_per_day = rental_price_per_day
+        self.__rental_price_per_day = rental_price_per_day
+
+    def get_rental_price_per_day(self):
+        return self.__rental_price_per_day
+
+    def set_rental_price_per_day(self,price):
+        if price > 0:
+            self.__rental_price_per_day = price
+        else:
+            print("price can't be nigative")
+
 
     def display_info(self):
-        print(f"{self.brand} {self.model}, Year:{self.year}, Rental Price: ${self.rental_price_per_day}/day")
+        print(f"{self.brand} {self.model}, Year:{self.year}, Rental Price: ${self.get_rental_price_per_day()}/day")
 
     def calculate_rental_cost(self, days):
-        return self.rental_price_per_day * days
+        return self.get_rental_price_per_day() * days
 
 
    
@@ -27,7 +31,7 @@ class Car(Vehicle):
         super().__init__(brand, model, year, rental_price_per_day)
 
     def display_info(self):
-        print(f"{self.brand} {self.model}, Year:{self.year},Seats: {self.seating_capacity},Rental Price: ${self.rental_price_per_day}/day")
+        print(f"{self.brand} {self.model}, Year:{self.year},Seats: {self.seating_capacity},Rental Price: ${self.get_rental_price_per_day()}/day")
 
 
 
@@ -39,7 +43,7 @@ class Bike(Vehicle):
         super().__init__(brand, model, year, rental_price_per_day)
     
     def display_info(self):
-        print(f"{self.brand} {self.model}, Year:{self.year},Engine: {self.engine_capacity}, Rental Price: ${self.rental_price_per_day}/day")
+        print(f"{self.brand} {self.model}, Year:{self.year},Engine: {self.engine_capacity}, Rental Price: ${self.get_rental_price_per_day()}/day")
 
 
     
