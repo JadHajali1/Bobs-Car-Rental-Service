31413734863c3f4b1383ca3672ae0493d4ffd3b9
JadHajali1 <jadhajali773@gmail.com>
2025-02-28 16:39:38 +0200

    Create constructor for type class

--- car_rent.py
+++ car_rent.py
@@ -1,4 +1,4 @@
-class CustomerRents:
+class vehicle:
 
     brand = ""
     model = ""
@@ -10,3 +10,19 @@ class CustomerRents:
         self.model = model
         self.year = year
         self.rental_price_per_day = rental_price_per_day
+
+   
+
+
+class type:
+    seating_capacity = 0
+    engine_capacity = ""
+
+    def __init__(self, seating_capacity, engine_capacity):
+        self.seating_capacity = seating_capacity
+        self.engine_capacity = engine_capacity
+        
+
+
+
+    
