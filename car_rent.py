da39113b2ae3a73d7a56f63154fae83e26be7eb3
JadHajali1 <jadhajali773@gmail.com>
2025-02-28 15:48:03 +0200

    Create constructor

--- car_rent.py
+++ car_rent.py
@@ -5,4 +5,8 @@ class CustomerRents:
     year = 0
     rental_price_per_day = 0
 
-    
+    def __init__(self, brand, model, year, rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.rental_price_per_day = rental_price_per_day
